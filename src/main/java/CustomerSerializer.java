e47d0c88aa541cbee2826b3ac7c5eff90ccbbbdb
yassine <yeladlouni@gmail.com>
2023-09-06 09:45:48 +0100

    customer producer example

--- src/main/java/CustomerSerializer.java
+++ src/main/java/CustomerSerializer.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class CustomerSerializer {
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.util.Map;
+
+public class CustomerSerializer implements Serializer {
+    @Override
+    public void configure(Map configs, boolean isKey) {
+        Serializer.super.configure(configs, isKey);
+    }
+
+    @Override
+    public byte[] serialize(String s, Object o) {
+        Customer customer = (Customer) o;
+        int id = customer.getId();
+        String firstName = customer.getFirstName();
+        String lastName = customer.getLastName();
+        int age = customer.getAge();
+        char gender = customer.getGender();
+        double salary = customer.getSalary();
+
+        int bytesSize = 4 + (firstName.length() + lastName.length()) * 8 + 4 + 8 + 16;
+        byte[] bytes = new byte[bytesSize];
+
+        return new byte[0];
+    }
+
+    @Override
+    public byte[] serialize(String topic, Headers headers, Object data) {
+        return Serializer.super.serialize(topic, headers, data);
+    }
+
+    @Override
+    public void close() {
+        Serializer.super.close();
+    }
 }
--- src/main/java/ProducerExample.java
+++ src/main/java/ProducerExample.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class ProducerExample {
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import scala.sys.Prop;
+import java.util.Properties;
+import java.util.Random;
+import java.util.UUID;
+
+public class ProducerExample {
+    public ProducerExample() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "customer-producer");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, CustomerSerializer.class.getName());
+
+        KafkaProducer<String, Customer> producer = new KafkaProducer<>(props);
+
+        String topic = "customers";
+
+        for (int i = 0; i < 1000000; i++) {
+            Customer customer = new Customer(
+                    i,
+                    "first_name_"+i,
+                    "last_name_"+i,
+                    (int)new Random().nextGaussian()*(60-18+1)+18,
+                    (new Random().nextBoolean()) ?'F':'M',
+                    new Random().nextDouble()*(50000-2600+1)+2600
+            );
+
+            String key = UUID.randomUUID().toString();
+
+            ProducerRecord<String, Customer> record = new ProducerRecord<>(topic, key, customer);
+            producer.send(record);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ProducerExample();
+    }
 }
